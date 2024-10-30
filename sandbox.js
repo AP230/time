8d78ef870c12806850ea76e232236f1d5ee60b57
ashley <126aperezmorales@frhsd.com>
2024-10-30 13:06:30 -0400

    second

---
+++ sandbox.js
@@ -0,0 +1,15 @@
+const d = new Date();
+let hour = d.getHours();
+hour = 12
+if(hour < 12);{
+    document.getElementById ("Greetings"). innertext = "Goodnight";
+    document.getElementById ("Pictures"). src = 
+    document.getElementById ("Greetings"). innertext = "Goodnight";
+   }   {
+    document.getElementById ("Greetings"). innertext = "Goodnight";
+    }
+
+    //let GreetingElement = document.getElementById ("Greetings");
+   // console.log(greetingElement);
+
+ 
