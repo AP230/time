ee9bba16261fe3a5b99d06e301279dbaf25562c3
ashley <126aperezmorales@frhsd.com>
2024-11-01 12:52:12 -0400

    Third

--- sandbox.js
+++ sandbox.js
@@ -1,15 +1,30 @@
+// Create new Date object
 const d = new Date();
+// Store the value of getHours in variable
 let hour = d.getHours();
-hour = 12
-if(hour < 12);{
-    document.getElementById ("Greetings"). innertext = "Goodnight";
-    document.getElementById ("Pictures"). src = 
-    document.getElementById ("Greetings"). innertext = "Goodnight";
-   }   {
-    document.getElementById ("Greetings"). innertext = "Goodnight";
-    }
+// hour = 15;
+// hour = 20;
+// hour = 2;
+// console.log(hour);
+// Create greeting variable
+let greetingText = document.getElementById("greeting-text");
 
-    //let GreetingElement = document.getElementById ("Greetings");
-   // console.log(greetingElement);
-
- 
+if (hour < 12) {
+  // if hour is less than 12pm
+  greetingText.textContent = "Good Morning!";
+  document.querySelector(".night").classList.add("d-none");
+  document.querySelector(".afternoon").classList.add("d-none");
+} else if (hour < 17) {
+  // if hour is less than 17:00 (but greater than 12pm)
+  greetingText.textContent = "Good Afternoon!";
+  document.getElementsByTagName("body")[0].classList.add("noon-bg");
+  greetingText.parentElement.classList.add("noon-gradient");
+  document.querySelector(".morning").classList.add("d-none");
+  document.querySelector(".night").classList.add("d-none");
+} // hour is greater than 17:00
+else {
+  greetingText.textContent = "Good Night!";
+  greetingText.parentElement.classList.add("night-gradient");
+  document.querySelector(".morning").classList.add("d-none");
+  document.querySelector(".afternoon").classList.add("d-none");
+}
